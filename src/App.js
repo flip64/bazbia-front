69ed5c598af5aca33df9c71d3706cf4786dfd9be
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-28 16:04:04 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Topbar from "./components/Topbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <Topbar cartCount={2} wishlistCount={5} />
+      {/* بقیه محتوای سایت */}
+
       </header>
     </div>
   );
